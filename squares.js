e46983f1457d5cc2f131542893bcd7e89862ed8c
Laarni Astrid <laarni.astrid@gmail.com>
2016-06-01 01:16:23 -0600

    completed - you're a square - training

---
+++ squares.js
@@ -0,0 +1,12 @@
+/*
+www.codewars.com
+Javascript training
+Title: You're a square!
+*/
+
+// Finished code
+var isSquare = function(n){
+  var temp = Math.sqrt(n);
+  if (temp % 1 === 0) { return true; }
+  else { return false; }
+}

09d658184bbe73b4879ebfb36b0133bead4eba1d
Laarni Astrid <laarni.astrid@gmail.com>
2016-06-01 01:32:23 -0600

    update 01 to completed code

--- oddInt.js
+++ oddInt.js
@@ -17,3 +17,16 @@ function findOdd(A) {
     count = 0;
   }
 }
+
+// Updated 01
+function findOdd(A) {
+  var count = 0;
+  for (var i = 0; i < A.length; i++) {
+    var temp = A[i];
+    A.forEach(function(entry) {
+      if (A[i] === entry) { count++; }
+    })
+    if (count % 2 !== 0) { return temp; }
+    count = 0;
+  }
+}

7c19e08c39e24d1f458adc9b5d361f759be682cc
Laarni Astrid <laarni.astrid@gmail.com>
2016-06-03 01:19:45 -0600

    added nex update

--- Javascript_Kata/alphabetPos.js
+++ Javascript_Kata/alphabetPos.js
@@ -5,3 +5,39 @@ Title: Replace With Alphabet Position
 */
 
 // Finished code
+function alphabetPosition(text) {
+  var temp = [];
+  var alpha = [null, 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  text = text.replace(/[,.!'"]/g, '')
+    .split(' ')
+    .join('')
+    .split('')
+    .map(function(entry) {
+      return entry.toLowerCase();
+    })
+  text.forEach(function(entry) {
+    alpha.forEach(function(value, index) {
+      if (entry === value) {
+        temp.push(index);
+      }
+    })
+  })
+  return temp.join(' ');
+}
+
+// Updated 01
+function alphabetPosition(text) {
+  var alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  return text.replace(/[^A-Za-z]/g, '')
+    .toLowerCase()
+    .split(' ')
+    .join('')
+    .split('')
+    .map(function(entry) {
+      var temp = null;
+      alpha.forEach(function(value, i) {
+        if (entry === value) { temp = i + 1; }
+      });
+      return temp;
+  }).join(' ');
+}

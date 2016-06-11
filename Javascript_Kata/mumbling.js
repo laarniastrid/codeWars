5f44cbc2b08cc0006cdf82f75aedfe95b42cfaf3
Laarni Astrid <laarni.astrid@gmail.com>
2016-06-11 01:07:33 -0600

    completed

--- Javascript_Kata/mumbling.js
+++ Javascript_Kata/mumbling.js
@@ -5,3 +5,17 @@ Title: Unique in Order
 */
 
 // Finished code
+function accum(s) {
+  var count = 1;
+  return s.split('')
+    .map((entry) => {
+      var str = '';
+      for (var i = 0; i < count; i++) {
+        if (i === 0) { str += entry.toUpperCase(); }
+        else { str += entry.toLowerCase(); }
+      }
+      count++;
+      return str;
+  })
+    .join('-')
+}

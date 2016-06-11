6a5104bf4a908eb005f6fdaf11bdc5b0ca3e0158
Laarni Astrid <laarni.astrid@gmail.com>
2016-06-11 01:53:44 -0600

    completed kata

--- Javascript_Kata/insertDashes.js
+++ Javascript_Kata/insertDashes.js
@@ -5,3 +5,15 @@ Title: Unique in Order
 */
 
 // Finished code
+function insertDash(num) {
+  var str = '';
+  num = num.toString();
+  num.split('').forEach((entry, i) => {
+    if (num[i + 1] == null) {
+      str += entry;
+    } else {
+      str += entry % 2 !== 0 && num[i + 1] % 2 !== 0 ? entry + '-' : entry;
+    }
+  })
+  return str;
+}
